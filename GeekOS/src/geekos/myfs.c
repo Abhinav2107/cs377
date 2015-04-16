2e8b214cd3a595f02b0ef1ee8ba94ff2b457d171
cs296_g20 <amangour30@gmail.com>
2015-04-16 15:35:35 +0530

    Read, FSTAT added

--- GeekOS/src/geekos/myfs.c
+++ GeekOS/src/geekos/myfs.c
@@ -13,6 +13,8 @@
 #include <geekos/projects.h>
 
 #define PAGEFILE_FILENAME "/pagefile.bin"
+#define NUM_BLOCKS_FILE 121
+#define BLOCK_SIZE 512
 
 int debugmyfs = 0;
 #define Debug(args...) if (debugmyfs) Print("myfs: " args)
@@ -34,8 +36,9 @@ int Allocate_Block(struct Block_Device * dev) {
 }
 
 static struct myfs_directoryEntry *myfs_Lookup(struct Mount_Point *mountPoint,
-                                   struct superBlock *sblock,
-                                   const char *path) {
+                                   const char *path) {;
+
+
     return 0;
 }
 
@@ -45,10 +48,67 @@ static void myfs_Register_Paging_File(struct Mount_Point *mountPoint, struct sup
 
 }
 
-
+static int myfs_FStat(struct File * file, struct VFS_File_Stat * stat) {
+	stat->size = (int)file->endPos;
+	stat->isDirectory = 0;
+	stat->isSetuid = 0;
+	stat->acls[0].permission = file->mode;
+	return 0;
+}
 
 
 static int myfs_Read(struct File * file, void * buf, ulong_t numBytes) {
+
+
+	if(!(file->mode & O_READ)) return EACCESS;
+	myfs_Seek(file,0);
+	//if(file->mode && O_READ) return EACCESS;	
+	
+	struct myfs_File f;
+	Block_Read(file->mountPoint->dev,((struct FCB_Data*)file->fsData)->blockno,&f);
+	if((int)file->filePos+(int)numBytes>f.fileSize) return -22;
+
+	ulong_t bytesRead=0;
+	int curPos=file->filePos;
+	int curBlock = curPos/512;
+	int already_read = curPos%512;
+	char * tempbuf = Malloc(512);
+	//Print("block num: %d\n",f.fmt[curBlock]);
+	Block_Read(file->mountPoint->dev,f.fmt[curBlock],tempbuf);
+	curBlock++;
+	int i;
+
+	for(i=already_read;i<512 && bytesRead < numBytes ; i++){
+			((char*)buf)[bytesRead++]=tempbuf[i];
+	}
+	Free(tempbuf);
+	while(1){
+			if((int)numBytes-(int)bytesRead < 512) break;
+			Block_Read(file->mountPoint->dev,f.fmt[curBlock++],&((char*)buf)[bytesRead]);
+			bytesRead+=512;
+	}
+	char * tempbuf2=Malloc(512);
+	char myarr[512];
+	int h;
+	//for(h=0;h<512;h++) myarr[h]='a'+(h)%25;
+	if((int)numBytes-(int)bytesRead > 0){
+					
+					//int jk=0;	
+					Block_Read(file->mountPoint->dev,f.fmt[curBlock++],tempbuf2);
+					//Print("block read: %d\n",y);
+					for(i=0;bytesRead<numBytes;i++){
+						((char*)buf)[bytesRead++]=tempbuf2[i];
+						//Print("tempbuf2 char: %c\n",tempbuf2[i]);
+						//Print("cur char: %c\n",((char*)buf)[bytesRead-1]);
+						//jk++;
+					}
+					//Print("jk: %d\n",jk);
+	}
+	else {
+		
+	}
+	Free(tempbuf2);
+	file->filePos += numBytes;
     return 0;
 }
 
@@ -60,22 +120,22 @@ static int myfs_Write(struct File * file, void * buf, ulong_t numBytes) {
     Block_Read(file->mountPoint->dev, fdata->blockno, &f);
     int start = file->filePos;
     int end = start + numBytes;
-    if(end > 121 * 512)
+    if(end > NUM_BLOCKS_FILE * BLOCK_SIZE)
     	return ENOSPACE;
     int index;
     int offset;
     int pos = start;
     int written = 0;
     while(1) {
-    	index = pos / 512;
-    	offset = pos % 512;
-    	int to_write = 512 - offset;
+    	index = pos / BLOCK_SIZE;
+    	offset = pos % BLOCK_SIZE;
+    	int to_write = BLOCK_SIZE - offset;
     	if(to_write > end - pos)
     		to_write = end - pos;
     	if(to_write <= 0)
     		break;
     	int blockno;
-    	if((index == 0 && file->endPos == 0) || index > ((int)file->endPos - 1)/ 512) {
+    	if((index == 0 && file->endPos == 0) || index > ((int)file->endPos - 1)/ BLOCK_SIZE) {
     		if((blockno = Allocate_Block(file->mountPoint->dev)) < 0)
     			break;
     		f.fmt[index] = blockno;
@@ -83,7 +143,7 @@ static int myfs_Write(struct File * file, void * buf, ulong_t numBytes) {
     	}
     	else
     		blockno = f.fmt[index];
-    	char temp[512];
+    	char temp[BLOCK_SIZE];
     	Block_Read(file->mountPoint->dev, blockno, temp);
     	memcpy(temp+offset, buf+pos-start, to_write);
     	Block_Write(file->mountPoint->dev, blockno, temp);
@@ -100,6 +160,30 @@ static int myfs_Write(struct File * file, void * buf, ulong_t numBytes) {
 }
 
 static int myfs_Seek(struct File * file, ulong_t pos) {
+	//seek to "pos"
+	//change the filePos to pos 
+	struct myfs_File f;
+    struct FCB_Data * fdata = (struct FCB_Data *)file->fsData;
+    
+    Block_Read(file->mountPoint->dev, fdata->blockno, &f);
+    int curr_filePos = file->filePos;
+    int seek_to = pos;
+    int endpos = file->endPos;
+
+    if((int)pos > endpos || (int)pos < 0)
+    	return EINVALID;
+    else if(pos != file->filePos)
+    {
+    	file->filePos = pos;
+    	//f.fileSize = file->endPos;
+    	//Block_Write(file->mountPoint->dev, fdata->blockno, &f);
+/*    	ulong_t curBlock = fdata->blockno;
+        uint_t i;
+        for (i = 0; i < pos; i += 512) {
+            curBlock = instance->fat[curBlock];
+        }
+        pfatFile->currBlock = curBlock;*/
+    }
     return 0;
 }
 
@@ -109,7 +193,7 @@ static int myfs_Close(struct File * file) {
 }
 
 static struct File_Ops myfs_File_Ops = {
-    0, //Fstat
+    &myfs_FStat,
     &myfs_Read,
     &myfs_Write,
     &myfs_Seek,
@@ -174,7 +258,7 @@ static int myfs_Open(struct Mount_Point * mountPoint, const char * path, int mod
 	int i;
 	*name = '\x00';
 	struct myfs_File f;
-	memset(&f, 0, 512);
+	memset(&f, 0, BLOCK_SIZE);
 	for(i = 0; i < 24; i++) {
 			if(*dir.files[i] == '\x00')
 					continue;
@@ -233,13 +317,19 @@ static int myfs_Create_Directory(struct Mount_Point * mountPoint, const char * p
 }
 
 
-static int Copy_Stat_For_myfs(struct VFS_File_Stat *stat, struct myfs_directoryEntry * entry){
-	
+/*static int Copy_Stat_For_myfs(struct VFS_File_Stat *stat, struct myfs_directoryEntry * entry)
+{
+	stat->size = entry->fileSize;
+    stat->isDirectory = entry->directory;
+
+    stat->isSetuid = entry->isSetUid;
+    memcpy(stat->acls, entry->acls, sizeof(stat->acls));
     return 0;
 
-}
+}*/
 
 static int myfs_Stat(struct Mount_Point * mountPoint, const char * path, struct VFS_File_Stat * stat) {
+
     return 0;
 }
 
@@ -272,15 +362,15 @@ struct Mount_Point_Ops myfs_Mount_Point_Ops = {
 };
 
 static int myfs_Format(struct Block_Device * blockDev) {
-	memset(&sb, 0, 512);
+	memset(&sb, 0, BLOCK_SIZE);
     sb.magic=69;
     sb.dsm=1;
     sb.root=2;
     sb.n_blocks=2048;
-    memset(dsm_array,0,512);
+    memset(dsm_array,0,BLOCK_SIZE);
     dsm_array[0]=(1<<31)|(1<<30)|(1<<29);
     struct myfs_directoryEntry root;
-    memset(&root,0,512);
+    memset(&root,0,BLOCK_SIZE);
     root.type=0;//Directory
     root.perms=7;
     Block_Write(blockDev,0,&sb);
